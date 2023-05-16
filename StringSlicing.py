9aa1b068b84671851f7222d6600d5c38a57cc55b
Akash-akp <akashakp0037@gmail.com>
2023-05-16 19:51:01 +0530

    String Slicing learned

---
+++ StringSlicing.py
@@ -0,0 +1,4 @@
+word = input("Enter a string : ")
+length = len(word)
+print("The length of the string \"{}\" is".format(word),length)
+print("The first 5 letter of string \"{}\" is".format(word),word[0:5]) # 0 index include 5 index donot include
