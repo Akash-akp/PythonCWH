46cb7d593d942440a406e93d0cc8844c439d51bc
Akash-akp <akashakp0037@gmail.com>
2023-05-16 22:48:27 +0530

    for loop done

---
+++ forloop.py
@@ -0,0 +1,10 @@
+colors = ["red","green","black","blue","orange"]
+
+for color in colors:
+    print(color)
+    for i in color:
+        print(i,end=",")
+    print()
+
+for k in range (1,101):
+    print(k)
