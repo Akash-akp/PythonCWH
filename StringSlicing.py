f18a43cbc68726aafee66157073e139924a91682
Akash-akp <akashakp0037@gmail.com>
2023-05-16 19:53:23 +0530

    String Slicing learned

--- StringSlicing.py
+++ StringSlicing.py
@@ -2,3 +2,4 @@ word = input("Enter a string : ")
 length = len(word)
 print("The length of the string \"{}\" is".format(word),length)
 print("The first 5 letter of string \"{}\" is".format(word),word[0:5]) # 0 index include 5 index donot include
+print("The string till last 2 letter of string \"{}\" is".format(word),word[0:-2])
