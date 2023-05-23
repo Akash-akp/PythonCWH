2a46222275bf8f77f57dcbc434850a7d6c3fd9bf
Akash-akp <akashakp0037@gmail.com>
2023-05-23 23:27:07 +0530

    learned tuple operation

---
+++ myTupleOperation.py
@@ -0,0 +1,15 @@
+tup = (1, 2, 3, 4, 5)
+# i have to change tup
+temp = list(tup)
+temp.append(6);
+tup = tuple(temp)
+print(tup)
+
+# concatenate the tuple
+
+tup1 = (1,2,5)
+tup2 = (7,8,9)
+t = tup1+tup2
+print(t)
+
+# everything will be same for tuple too
