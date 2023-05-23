12ba915533b7b51d229630719a8403a036d6618a
Akash-akp <akashakp0037@gmail.com>
2023-05-23 22:07:00 +0530

    learned list

---
+++ myList.py
@@ -0,0 +1,37 @@
+mark = [3,5,6,"Akash",True]
+# print(mark[0])
+# print(mark[3])
+# print(mark[4])
+
+"""
+print(mark[-3]) # Negative index
+print(mark[len(mark)-3]) # Positive index
+print(mark[5-3]) # Positive index
+print(mark[2]) # Positive index
+"""
+
+"""
+# condition : is 5 present in mark or not?
+if 5 in mark:
+    print("Yes")
+else:
+    print("No")
+"""
+
+"""
+# same thing apply on string
+if "kash" in "Akash":  
+    print("Yes")
+else:
+    print("No")
+"""
+
+"""
+# jump index
+print(mark[1:-1])
+print(mark[1:4])
+print(mark[0:4:2])
+"""
+
+li = [i for i in range(1,11)]
+print(li)
