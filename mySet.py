3432a42ccd6b210b5da24f670ddd1baa969f82d4
Akash-akp <akashakp0037@gmail.com>
2023-05-24 10:14:19 +0530

    set learned

---
+++ mySet.py
@@ -0,0 +1,13 @@
+s = {1,2,2,3,4,2,5}
+print(type(s),s)
+
+# set doesn't maintain order
+
+# d = {} # d is dictionary
+# print(type(d))
+
+# d = set() # empty set is declared in this way
+# print(type(d))
+
+for i in s:
+    print(i)
