b5388157e907b7071084e7406dbb0339c7f5418b
Akash-akp <akashakp0037@gmail.com>
2023-05-27 12:26:34 +0530

    import python file learned

---
+++ Akash.py
@@ -0,0 +1,9 @@
+def Welcome():
+    print("You are welcome!! Akash")
+
+def Thank():
+    print("Thank you!! Akash")
+
+Akash = "My name is Akash"
+if __name__ == '__main__':
+    Thank()
---
+++ myimportwork.py
@@ -0,0 +1,13 @@
+# from math import sqrt ,pi
+import pandas as pd
+from Akash import Welcome,Thank,Akash
+import math as m
+a = m.sqrt(9)+m.pi;
+print(a)
+print(dir(m))
+print(m.nan,type(m.nan))
+
+
+print(Akash)
+Thank()
+Welcome()
