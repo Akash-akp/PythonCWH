4b75a6698c320e85e7a3ba3eaf9659baf4bdeb72
Akash-akp <akashakp0037@gmail.com>
2023-06-05 13:07:25 +0530

    learned decorator

---
+++ myDecorator.py
@@ -0,0 +1,27 @@
+def greet(fx):
+    def mfx(*args,**kwargs):
+        print("Hello!!! You are using this function.")
+        fx(*args,**kwargs)
+        print("Thanks you for using this function")
+
+    return mfx;
+
+
+@greet
+def Hello():
+    print("Hello World")
+
+
+def add(a, b):
+    print(a + b)
+
+
+def name():
+    print("Hii My name is Akash")
+
+
+Hello()
+
+greet(name)()
+
+greet(add)(3,4)
